df6cab8c27bb807038a98edc4e0f07ff30c1211d
Sarthak <sarthakbhatt@live.com>
2024-04-02 03:47:11 +0530

    making get requests using cloudflare

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,17 @@ export interface Env {}
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return Response.json({ msg: 'Hello World!', sentBy: 'Sarthak Bhatt' });
+		console.log(request.body);
+		console.log(request.headers);
+
+		if (request.method === 'GET') {
+			return Response.json({
+				message: 'you sent a get request',
+			});
+		} else {
+			return Response.json({
+				message: 'you did not send a get request',
+			});
+		}
 	},
 };
